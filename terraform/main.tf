6aab5212c9f9407d307c35dc496f64268f8e1789
Ben Hummerstone <bhummerstone@users.noreply.github.com>
2024-03-08 11:10:36 +0000

    specifying newest versions for providers

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "~> 3.0.0"
+      version = "3.94.0"
     }
     random = {
       source = "hashicorp/random"
-      version = "~> 3.0.0"
+      version = "3.6.0"
     }
   }
 }
